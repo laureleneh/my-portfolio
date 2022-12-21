b14b4bdfbd90a0a3b8cd14b3fa0dee61f48730cd
laureleneh <121177279+laureleneh@users.noreply.github.com>
2022-12-21 15:28:12 +0100

    Delete Projet Joy/JS directory

--- Projet Joy/JS/carte.js
+++
@@ -1,6 +0,0 @@
-const retour = document.querySelector(".retour");
-
-// toggle 'scroll to top' based on scroll position
-window.addEventListener('scroll', e => {
-  retour.style.display = window.scrollY > 1200 ? 'block' : 'none';
-});
